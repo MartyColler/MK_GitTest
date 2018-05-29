b95c16326cc919648ed152decdc3154488859820
Martin Koller <martin.koller@gmail.com>
2018-05-29 14:04:06 +0200

    Zkouška

---
+++ cs test 2/cs test 2/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace cs_test_2
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
